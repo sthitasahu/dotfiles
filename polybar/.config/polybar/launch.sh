0e0af47eceece36cba9556bfbfcb80dd45e10501
sthitasahu <sthitasahu074@gmail.com>
2025-06-03 00:05:34 +0530

    'Updated config for polybar'

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -x polybar >/dev/null; do sleep 0.5; done
 
 # Launch Polybar
 # Change 'example' to the name of your Polybar config if needed (typically the bar section name)
-polybar toph &
+polybar example &
 
 echo "Polybar launched"
 
